6525bc47b28567fad4acd63d77c8d3e5543417f8
NgocCanh01 <canhdao2001@gmail.com>
2023-04-01 17:00:51 +0700

    khong thay doi

--- src/main/java/com/example/shoppy/activities/MainActivity.java
+++ src/main/java/com/example/shoppy/activities/MainActivity.java
@@ -38,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
         anhXa();//V1
         actionBar();//V2
         actionViewFlipper();//add QC cho viewFlipp
-        //Up vào dev_Canh
-        //main up dev_canh
+
 
 
     }
