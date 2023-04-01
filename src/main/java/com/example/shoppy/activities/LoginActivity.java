fe60091f8bbd260dd65a577c2971ef8530fbc91d
NgocCanh01 <canhdao2001@gmail.com>
2023-04-01 10:58:59 +0700

    Initial commit

---
+++ src/main/java/com/example/shoppy/activities/LoginActivity.java
@@ -0,0 +1,176 @@
+package com.example.shoppy.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.shoppy.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView createNewAccount,forgotPassword;
+    EditText inputEmail, inputPassword;
+    Button btnLogin;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth,firebaseAuth;
+    FirebaseUser mUser;
+    ImageView btnGoogle;
+    ImageView btnFacebook;
+    ImageView btnGit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        createNewAccount = findViewById(R.id.createNewAccount);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnGoogle = findViewById(R.id.btnGoogle);
+        btnFacebook = findViewById(R.id.btnFacebook);
+        forgotPassword = findViewById(R.id.forgotPassword);
+        btnGit = findViewById(R.id.btnGit);
+
+
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+
+
+        createNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perforLogin();
+            }
+        });
+
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (LoginActivity.this, GoogleSignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(LoginActivity.this, FacebookAuthActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        btnGit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,  GitAuthActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                progressDialog.setTitle("Sending...");
+                progressDialog.show();
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                progressDialog.cancel();
+                                Toast.makeText(LoginActivity.this,"Email sent", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.cancel();
+                        Toast.makeText(LoginActivity.this, e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    private void perforLogin() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (!email.matches(emailPattern)) {
+            inputEmail.setError("Enter Connext Email");
+        } else if (password.isEmpty() || password.length() < 6) {
+            inputPassword.setError("Enter Proper Password");
+        } else {
+
+            progressDialog.setMessage("Please wait while Login .....");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                    }else {
+
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this,"" + task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+
+        }
+         }
+
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
---
+++ src/main/java/com/example/shoppy/activities/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.shoppy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
+import com.example.shoppy.R;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar tbMain;
+    ViewFlipper vfMain;
+    RecyclerView rcSanPham;
+    NavigationView nvMain;
+    ListView lvMain;
+    DrawerLayout drawerLayoutMain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        anhXa();//V1
+        actionBar();//V2
+        actionViewFlipper();//add QC cho viewFlipp
+    }
+
+    private void actionViewFlipper() {
+        List<String> mangQuangCao= new ArrayList<>();
+        mangQuangCao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
+        mangQuangCao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-HC-Tra-Gop-800-300.png");
+        mangQuangCao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-big-ky-nguyen-800-300.jpg");
+        //Dùng thư viện glider đưa hình ảnh vào imageView
+        for(int i=0; i<mangQuangCao.size(); i++){
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext()).load(mangQuangCao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            vfMain.addView(imageView);
+        }
+        vfMain.setFlipInterval(3000);
+        vfMain.setAutoStart(true);
+        //set Animation cho viewFlipp
+        Animation slideIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_right);
+        Animation slideOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_out_right);
+        vfMain.setInAnimation(slideIn);
+        vfMain.setOutAnimation(slideOut);
+    }
+
+    private void actionBar() {
+        setSupportActionBar(tbMain);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        tbMain.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        tbMain.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayoutMain.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void anhXa() {
+        tbMain = findViewById(R.id.tbMain);
+        vfMain = findViewById(R.id.vfMain);
+        rcSanPham = findViewById(R.id.rcSanPham);
+        nvMain = findViewById(R.id.nvMain);
+        lvMain = findViewById(R.id.lvMain);
+        drawerLayoutMain = findViewById(R.id.drawerLayoutMain);
+    }
+}
